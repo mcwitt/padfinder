f03273a192d6a94e44b0c1f195fafc18ad0c3505
Matt Wittmann <mcwitt@gmail.com>
2017-03-18 11:04:20 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+package = 'padfinder'
+version = '0.1'
+
+setup(name=package,
+      version=version,
+      description="scraper and data model for apartment hunting",
+      url='https://github.com/mcwitt/padfinder',
+      install_requires=[
+          'scrapy',
+          'sqlalchemy'
+      ])
