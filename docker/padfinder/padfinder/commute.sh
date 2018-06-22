5f3813bb6fb40179b8689313adcc0bc1418e6a49
Matt Wittmann <mwittmann@stitchfix.com>
2018-06-22 11:48:14 -0700

    Change default params for Seattle

--- docker/padfinder/padfinder/commute.sh
+++ docker/padfinder/padfinder/commute.sh
@@ -2,12 +2,23 @@
 
 while true; do
     python update_commutes.py \
-           --destination "Mission Cliffs, San Francisco, USA" \
-           --destination "California Academy of Sciences, San Francisco, USA" \
+           --destination "University of Washington Station, Seattle, USA" \
+           --destination "Capitol Hill Station, Seattle, USA" \
+           --destination "Westlake Station, Seattle, USA" \
+           --destination "University Street Station, Seattle, USA" \
+           --destination "Pioneer Square Station, Seattle, USA" \
+           --destination "Intl. District/Chinatown Station, Seattle, USA" \
+           --destination "Stadium Station, Seattle, USA" \
+           --destination "SODO Station, Seattle, USA" \
+           --destination "Beacon Hill Station, Seattle, USA" \
+           --destination "Mount Baker Station, Seattle, USA" \
+           --destination "Columbia City Station, Seattle, USA" \
+           --destination "Othello Station, Seattle, USA" \
+           --destination "Rainier Beach Station, Seattle, USA" \
+           --destination "Stone Gardens Seattle, Seattle, USA" \
+           --destination "Seattle Bouldering Project, Seattle, USA" \
            --transit-mode walking \
            --transit-mode bicycling \
-           --transit-mode transit \
-	   --transit-mode driving \
            --download-delay 10
     sleep 900
 done
